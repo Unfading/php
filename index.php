cbe6602d6a9117be0e5cb0fc280fa0288579da1b
Unfading <john-stud-spb@yandex.ru>
2017-01-19 00:20:46 +0300

    + метод query() класса Db

--- index.php
+++ index.php
@@ -3,5 +3,5 @@
 require __DIR__ . '/autoload.php';
 
 $db = new \App\Db();
-$res = $db->execute('CREATE TABLE foo (id SERIAL) ');
-var_dump($res);
+$data = $db->query('SELECT * FROM users');
+var_dump($data);
