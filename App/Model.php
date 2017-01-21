3ec0e1c6403403bfa27735bb639527e8caec648d
Unfading <dredoras071987>
2017-01-21 23:40:13 +0300

    ! ActiveRecord

--- App/Model.php
+++ App/Model.php
@@ -5,15 +5,46 @@ namespace App;
 abstract class Model
 {
     const TABLE = '';
+
+    public $id;
+
     public static function findAll()
     {
         $db = Db::instance();
         return $db->query('SELECT * FROM ' . static::TABLE, static::class);
 
     }
-    public static function findById($id)
+    /*public static function findById($id)
     {
         $db = Db::instance();
         return $db->query('SELECT * FROM ' . static::TABLE . ' WHERE id = ' . $id, static::class);
+    }*/
+
+    public function isNew()
+    {
+        return empty($this->id);
+    }
+
+    public function insert()
+    {
+        if(!$this->isNew()){
+            return;
+        }
+
+        $columns = [];
+        $values = [];
+        foreach ($this as $k => $value){
+            if ('id' == $k){
+                continue;
+            }
+            $columns[] = $k;
+            $values[':' . $k] = $value;
+        }
+        $sql = 'INSERT INTO ' . static::TABLE . '
+            (' . implode(',', $columns) . ')
+        VALUES
+        (' . implode(',', array_keys($values)) . ')' ;
+        $db = Db::instance();
+        $db->execute($sql, $values);
     }
 }
--- index.php
+++ index.php
@@ -2,12 +2,8 @@
 
 require __DIR__ . '/autoload.php';
 
-$users = \App\Models\Users::findAll();
-
-var_dump($users);
-
-echo '<hr>';
-
-$user = \App\Models\Users::findById(3);
-
-var_dump($user);
+$user = new \App\Models\Users();
+$user->firstName = 'Eugene';
+$user->lastName = 'Sobol';
+$user->age = 29;
+$user->insert();
